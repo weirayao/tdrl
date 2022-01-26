dca465c471632b5f9acf1adf8f93c16d6e317b7b
weirayao <weiranyao@outlook.com>
2022-01-26 01:41:44 -0500

    Add run.sh

---
+++ scripts/run.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+# Note: change the dataset path and log path in config files in configs folder
+# Stationary dynamics
+python train_stationary.py -e stationary_2lag --seed 770
+# Changing dynamics
+python train_change.py -e change_5 --seed 770
+# Modular changes
+# This one may need some tuning
+python train_modular.py -e modular_5 --seed 770
